442236bd66d3518b881dcb6bca92278124ce9af7
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-04-19 12:40:49 +0200

    chore: styled package

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,4 @@
-.getEncodeKey <- function(){
-
+.getEncodeKey <- function() {
   encode_list <- list(
     input = c("list", "(", ")", "\"", ",", " ", "c", ":", "!", "&", "|"),
     output = c("$LIST$", "$LB", "$RB$", "$QUOTE$", "$COMMA$", "$SPACE$", "$C$", "$COLON$", "$EXCL$", "$AND$", "$OR$")
@@ -7,25 +6,23 @@
   return(encode_list)
 }
 
-.decode_tidy_eval <- function(input_string, encode_key){
+.decode_tidy_eval <- function(input_string, encode_key) {
   encode_vec <- setNames(encode_key$input, encode_key$output)
   output_string <- str_replace_all(input_string, fixed(encode_vec))
   return(output_string)
 }
 
-.remove_list <- function(string_decoded){
-
+.remove_list <- function(string_decoded) {
   string_decoded %>%
     str_replace_all(pattern = fixed("list("), replacement = "") %>%
     str_sub(end = -2)
 }
 
-.format_encoded_expr <- function(expr){
-
+.format_encoded_expr <- function(expr) {
   return(out)
 }
 
-.remove_spaces <- function(expr){
+.remove_spaces <- function(expr) {
   out <- str_replace_all(expr, fixed(" "), "")
   return(out)
 }
