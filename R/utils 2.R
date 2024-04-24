59bf0b856dd1c80aba752af97462a6754553f246
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-04-24 13:24:42 +0200

    chore: improved error messages

---
+++ R/utils 2.R
@@ -0,0 +1,19 @@
+.getEncodeKey <- function(){
+
+  encode_list <- list(
+    input = c("list", "(", ")", "\"", ",", " ", "c", ":", "!", "&", "|"),
+    output = c("$LIST$", "$LB", "$RB$", "$QUOTE$", "$COMMA$", "$SPACE$", "$C$", "$COLON$", "$EXCL$", "$AND$", "$OR$")
+  )
+  return(encode_list)
+}
+
+.decode_tidy_eval <- function(input_string, encode_key){
+  encode_vec <- setNames(encode_key$input, encode_key$output)
+  output_string <- str_replace_all(input_string, fixed(encode_vec))
+  return(output_string)
+}
+
+.remove_spaces <- function(expr){
+  out <- str_replace_all(expr, fixed(" "), "")
+  return(out)
+}
