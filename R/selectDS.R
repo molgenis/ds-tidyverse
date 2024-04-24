2e4f56dd4982b030060c95cb4ae1b2622cea5362
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-04-24 13:24:42 +0200

    chore: improved error messages

--- R/selectDS.R
+++ R/selectDS.R
@@ -12,10 +12,8 @@
 #' @export
 #'
 selectDS <- function(.data, expr) {
-  ds_data <- eval(parse(text = .data), envir = parent.frame())
-  out <- .decode_tidy_eval(expr, .getEncodeKey())
-  string_to_eval <- paste0(.data, " %>% dplyr::select(", out, ")")
-  string_as_expr <- rlang::parse_expr(string_to_eval)
-  out <- eval_tidy(string_as_expr)
+  .check_data(.data)
+  tidy_select_args <- .decode_tidy_eval(expr, .getEncodeKey())
+  out <- .execute_tidyverse_function(.data, "select", tidy_select_args)
   return(out)
-}
+  }
