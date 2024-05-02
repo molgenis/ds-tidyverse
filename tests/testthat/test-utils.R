1f75641fee25979635c3ffbdcb9473df678a2e28
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-05-02 11:52:28 +0200

    chore: working on tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,8 +2,8 @@ library(stringr)
 
 test_that(".getEncodeKey returns the expected encoding key", {
   expected_encode_list <- list(
-    input = c("list", "(", ")", "\"", ",", " ", "c", ":", "!", "&", "|"),
-    output = c("$LIST$", "$LB$", "$RB$", "$QUOTE$", "$COMMA$", "$SPACE$", "$C$", "$COLON$", "$EXCL$", "$AND$", "$OR$")
+    input = c("(", ")", "\"", ",", " ", ":", "!", "&", "|", "'", "[", "]", "="),
+    output = c("$LB$", "$RB$", "$QUOTE$", "$COMMA$", "$SPACE$", "$COLON$", "$EXCL$", "$AND$", "$OR$", "$APO$", "$LSQ$", "$RSQ", "$EQU$")
   )
   actual_encode_list <- .getEncodeKey()
   expect_equal(actual_encode_list, expected_encode_list)
