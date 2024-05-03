022c71c581b6cda9e60e2039f8b5d7e44e4b53f3
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-05-03 12:27:46 +0200

    all tests working

--- tests/testthat/test-dsSelect.R
+++ tests/testthat/test-dsSelect.R
@@ -1,6 +1,23 @@
 library(dplyr)
 library(DSLite)
 library(dsBaseClient)
+library(purrr)
+library(cli)
+library(DSI)
+library(dsTidyverseClient)
+
+options(datashield.env=environment())
+data("mtcars")
+dslite.server <- newDSLiteServer(tables=list(mtcars=mtcars))
+data("logindata.dslite.cnsim")
+logindata.dslite.cnsim <- logindata.dslite.cnsim %>%
+  mutate(table = "mtcars")
+dslite.server$config(defaultDSConfiguration(include=c("dsBase", "dsTidyverse", "dsDanger")))
+dslite.server$assignMethod("selectDS", "selectDS")
+dslite.server$aggregateMethod("exists", "base::exists")
+dslite.server$aggregateMethod("classDS", "dsBase::classDS")
+dslite.server$aggregateMethod("lsDS", "dsBase::lsDS")
+conns <- datashield.login(logins = logindata.dslite.cnsim, assign=TRUE)
 
 test_that(".make_tidyselect_arg creates argument to pass to `eval_tidy", {
   input_string <- "asd, qwe, starts_with('test')"
@@ -71,19 +88,8 @@ test_that(".execute_tidyverse_function fails with correct message when unrecogni
     error = TRUE)
 })
 
-.encode_tidy_eval <- function(input_string, encode_key) {
-  encode_vec <- set_names(encode_key$output, encode_key$input)
-  output_string <- str_replace_all(input_string, fixed(encode_vec))
-}
-
-.wrap_assign_call <- function(tidy_select_as_string, newobj){
-  args_encoded <- .encode_tidy_eval(tidy_select_as_string, .getEncodeKey())
-  cally <- call("selectDS", "mtcars", args_encoded)
-  datashield.assign(conns, newobj, cally)
-}
-
-.check_cols_as_expected <- function(expected, newobj){
-  observed <- ds.colnames(newobj)[[1]]
+.check_cols_as_expected <- function(expected, df){
+  observed <- ds.colnames(df)[[1]]
   expected <- expected
   expect_equal(observed, expected)
 }
@@ -94,126 +100,144 @@ test_that(".execute_tidyverse_function fails with correct message when unrecogni
   datashield.assign(conns, "test", cally)
 }
 
-## ---- Log in -------------------------------------------------------------------------------------
-# data("mtcars")
-# dslite.server <- newDSLiteServer(tables=list(mtcars=mtcars))
-# data("logindata.dslite.cnsim")
-# logindata.dslite.cnsim <- logindata.dslite.cnsim %>%
-#   mutate(table = "mtcars")
-# dslite.server$config(defaultDSConfiguration(include=c("dsBase", "dsTidyverse", "dsDanger")))
-# dslite.server$assignMethod("selectDS", "selectDS")
-# conns <- datashield.login(logindata.dslite.cnsim, assign=TRUE)
+test_that("selectDS fails with correct error message ", {
+  expect_snapshot(
+    .wrap_assign_call_no_data("mpg:drat"),
+    error = TRUE)
+}) ## Wait until DSI updated
 
-## ---- Tests --------------------------------------------------------------------------------------
-# test_that("selectDS fails with correct error message ", {
-#   expect_snapshot(
-#     .wrap_assign_call_no_data("mpg:drat")
-#     error = TRUE)
-# })
 
 test_that("selectDS correctly passes : ", {
-  data("mtcars")
-  dslite.server <- newDSLiteServer(tables=list(mtcars=mtcars))
-  data("logindata.dslite.cnsim")
-  logindata.dslite.cnsim <- logindata.dslite.cnsim %>%
-    mutate(table = "mtcars")
-  dslite.server$config(defaultDSConfiguration(include=c("dsBase", "dsTidyverse", "dsDanger")))
-  dslite.server$assignMethod("selectDS", "selectDS")
-  conns <- datashield.login(logindata.dslite.cnsim, assign=TRUE)
-    select_arg = "mpg:drat"
-    args_encoded <- .encode_tidy_eval(select_arg, .getEncodeKey())
-    cally <- call("selectDS", "mtcars", args_encoded)
-    datashield.assign(conns, "test2", cally)
-    observed <- ds.colnames("test2")[[1]]
-    expected <- c("mpg", "cyl", "disp", "hp", "drat")
-    expect_equal(observed, expected)
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(mpg:drat),
+    newobj = "mpg_drat")
+  expected <- c("mpg", "cyl", "disp", "hp", "drat")
+  .check_cols_as_expected(expected, "mpg_drat")
 })
 
-# test_that(".execute_tidyverse_function correctly passes `starts_with`", {
-#   select_arg <- "starts_with('m')"
-#   args_encoded <- .encode_tidy_eval(select_arg, .getEncodeKey())
-#   cally <- call("selectDS", "mtcars", args_encoded)
-#   datashield.assign(conns, "starts", cally)
-#   observed <- ds.colnames("starts")[[1]]
-#   expected <- "mpg"
-#   expect_equal(observed, expected)
-# })
-#
-# test_that(".execute_tidyverse_function correctly passes `ends_with`", {
-#   select_arg <- "ends_with('m')"
-#   .wrap_assign_call(select_arg, "ends")
-#   .check_cols_as_expected("am", "ends")
-# })
-#
-# test_that(".execute_tidyverse_function correctly passes `matches`", {
-#   select_arg <- "matches('[aeiou]')"
-#   .wrap_assign_call(select_arg)
-#   .check_cols_as_expected(c("disp", "drat", "qsec", "am", "gear", "carb"))
-# })
-#
-# test_that(".execute_tidyverse_function correctly passes `everything`", {
-#   select_arg <- "everything()"
-#   .wrap_assign_call(select_arg)
-#   .check_cols_as_expected(colnames(mtcars))
-# })
-#
-# test_that(".execute_tidyverse_function correctly passes `last_col`", {
-#   select_arg <- "last_col()"
-#   .wrap_assign_call(select_arg)
-#   .check_cols_as_expected("carb")
-# })
-#
-# test_that(".execute_tidyverse_function correctly passes `group_cols`", {
-#   select_arg <- "group_cols()"
-#   .wrap_assign_call(select_arg)
-#   .check_cols_as_expected(character(0))
-# })
-#
-# test_that(".execute_tidyverse_function correctly passes strings with '&'", {
-#   select_arg <- "starts_with('c') & ends_with('b')"
-#   .wrap_assign_call(select_arg)
-#   .check_cols_as_expected("carb")
-# })
-#
-# test_that(".execute_tidyverse_function correctly passes strings with '!'", {
-#   select_arg <- "!mpg"
-#   .wrap_assign_call(select_arg)
-#   .check_cols_as_expected(c("cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb"))
-# })
-#
-# test_that(".execute_tidyverse_function correctly passes strings with '|'", {
-#   select_arg <- "starts_with('c') | ends_with('b')"
-#   .wrap_assign_call(select_arg)
-#   .check_cols_as_expected(c("cyl", "carb"))
-# })
-#
-# test_that(".execute_tidyverse_function correctly passes `strings with `all_of`", {
-#   select_arg <- "all_of(c('mpg','cyl'))"
-#   .wrap_assign_call(select_arg)
-#   .check_cols_as_expected(c("mpg", "cyl"))
-# })
-#
-# test_that(".execute_tidyverse_function correctly passes strings with `any_of`", {
-#   select_arg <- "any_of(c('mpg','cyl'))"
-#   .wrap_assign_call(select_arg)
-#   .check_cols_as_expected(c("mpg", "cyl"))
-# })
-#
-# test_that(".execute_tidyverse_function correctly passes complex strings", {
-#   select_arg <- "(starts_with('c') & ends_with('b')) | contains('ra') | gear:carb"
-#   .wrap_assign_call(select_arg)
-#   .check_cols_as_expected(c("carb", "drat", "gear"))
-# })
-#
-# test_that(".execute_tidyverse_function correctly passes strings with `where`", {
-#   select_arg <- "where(is.numeric)"
-#   .wrap_assign_call(select_arg)
-#   .check_cols_as_expected(colnames(mtcars))
-# })
-#
-# test_that(".execute_tidyverse_function correctly passes strings with '='", {
-#   select_arg <- "test = mpg"
-#   .wrap_assign_call(select_arg)
-#   .check_cols_as_expected("test")
-# })
+test_that(".execute_tidyverse_function correctly passes `starts_with`", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(starts_with("m")),
+    newobj = "starts")
+  expected <- "mpg"
+  .check_cols_as_expected(expected, "starts")
+})
 
+test_that(".execute_tidyverse_function correctly passes `ends_with`", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(ends_with('m')),
+    newobj = "ends")
+  expected <- "am"
+  .check_cols_as_expected(expected, "ends")
+})
+
+test_that(".execute_tidyverse_function correctly passes `matches`", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(matches('[aeiou]')),
+    newobj = "matches")
+  expected <- c("disp", "drat", "qsec", "am", "gear", "carb")
+  .check_cols_as_expected(expected, "matches")
+})
+
+test_that(".execute_tidyverse_function correctly passes `everything`", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(everything()),
+    newobj = "everything")
+  expected <- colnames(mtcars)
+  .check_cols_as_expected(expected, "everything")
+})
+
+test_that(".execute_tidyverse_function correctly passes `last_col`", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(last_col()),
+    newobj = "last")
+  expected <- "carb"
+  .check_cols_as_expected(expected, "last")
+})
+
+test_that(".execute_tidyverse_function correctly passes `group_cols`", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(group_cols()),
+    newobj = "group")
+  expected <- character(0)
+  .check_cols_as_expected(expected, "group")
+})
+
+test_that(".execute_tidyverse_function correctly passes strings with '&'", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(starts_with('c') & ends_with('b')),
+    newobj = "and")
+  expected <- "carb"
+  .check_cols_as_expected(expected, "and")
+})
+
+test_that(".execute_tidyverse_function correctly passes strings with '!'", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(!mpg),
+    newobj = "not")
+  expected <- c("cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb")
+  .check_cols_as_expected(expected, "not")
+})
+
+test_that(".execute_tidyverse_function correctly passes strings with '|'", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(starts_with('c') | ends_with('b')),
+    newobj = "or")
+  expected <- c("cyl", "carb")
+  .check_cols_as_expected(expected, "or")
+})
+
+test_that(".execute_tidyverse_function correctly passes `strings with `all_of`", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(all_of(c('mpg','cyl'))),
+    newobj = "all_of")
+  expected <- c("mpg", "cyl")
+  .check_cols_as_expected(expected, "all_of")
+})
+
+test_that(".execute_tidyverse_function correctly passes strings with `any_of`", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(any_of(c('mpg','cyl'))),
+    newobj = "any_of")
+  expected <- c("mpg", "cyl")
+  .check_cols_as_expected(expected, "any_of")
+})
+
+test_that(".execute_tidyverse_function correctly passes complex strings", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list((starts_with('c') & ends_with('b')) | contains('ra') | gear:carb),
+    newobj = "complex")
+  expected <- c("carb", "drat", "gear")
+  .check_cols_as_expected(expected, "complex")
+})
+
+test_that(".execute_tidyverse_function correctly passes strings with `where`", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(where(is.numeric)),
+    newobj = "where")
+  expected <- colnames(mtcars)
+  .check_cols_as_expected(expected, "where")
+})
+
+test_that(".execute_tidyverse_function correctly passes strings with '='", {
+  ds.select(
+    .data = "mtcars",
+    tidy_select = list(test = mpg, cyl, gear),
+    newobj = "equals")
+  expected <- c("test", "cyl", "gear")
+  .check_cols_as_expected(expected, "equals")
+})
