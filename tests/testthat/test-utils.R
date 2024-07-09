ce8879e159fe745857c8d06c30713e507de94685
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-07-09 15:21:26 +0200

    chore: actually add the correct file

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -56,61 +56,6 @@ test_that(".execute_with_error_handling fails with correct message when unrecogn
   )
 })
 
-test_that(".make_tidyselect_arg creates argument to pass to `eval_tidy", {
-  input_string <- "asd, qwe, starts_with('test')"
-  expected_string <- "test %>% dplyr::select(asd, qwe, starts_with('test'))"
-  observed_string <- .make_tidyselect_arg(.data = "test", fun = "select", tidy_select_args = input_string)
-  expect_equal(expected_string, observed_string)
-})
-
-test_that(".make_tidyselect_arg fails with correct message if attempt to use non permitted tidyverse serverside command", {
-  expect_error(
-    .execute_tidyverse_function("mtcars", "filter", mtcars_random_arg),
-    "\\b(permitted\\s+tidyverse\\s+functions\\s+within\\DataSHIELD)|Permitted\\s+functions\\s+are\\s+select|You\\s+have\\s+attempted\\s+to\\s+pass\\s+filter)\\b")
-})
-
-mtcars_good_arg <- "mpg, cyl, starts_with('g'), ends_with('b')"
-mtcars_good_str <- .make_tidyselect_arg(.data = "mtcars", fun = "select", tidy_select_args = mtcars_good_arg)
-mtcars_good_expr <- rlang::parse_expr(mtcars_good_str)
-
-test_that(".tidy_eval_handle_errors works where data and object exists", {
-  observed <- .tidy_eval_handle_errors(mtcars_good_expr, "mtcars")
-  expect_equal(colnames(observed), c("mpg", "cyl", "gear", "carb"))
-})
-
-mtcars_good_arg <- "mpg, cyl, starts_with('g'), ends_with('b')"
-mtcars_wrong_data_str <- .make_tidyselect_arg(.data = "data_not_here", fun = "select", tidy_select_args = mtcars_good_arg)
-mtcars_wrong_data_expr <- rlang::parse_expr(mtcars_wrong_data_str)
-
-test_that(".tidy_eval_handle_errors fails with correct message if object doesn't exist", {
-  expect_error(
-    .tidy_eval_handle_errors(mtcars_wrong_data_expr, "data_not_here"),
-    "`selectDS`\\s+returned\\s+the\\s+following\\s+error:|object\\s+'mtcars_wrong_data_expr'\\s+not\\s+found"
-  )
-})
-
-mtcars_missing_col_arg <- "all_of('test_col'), mpg, cyl, starts_with('g'), ends_with('b')"
-mtcars_missing_col_str <- .make_tidyselect_arg(.data = "mtcars", fun = "select", tidy_select_args = mtcars_missing_col_arg)
-mtcars_missing_col_expr <- rlang::parse_expr(mtcars_missing_col_str)
-
-test_that(".tidy_eval_handle_errors fails with correct message if column doesn't exist", {
-  expect_error(
-    .tidy_eval_handle_errors(mtcars_missing_col_expr, "mtcars"),
-    "`selectDS`\\s+returned\\s+the\\s+following\\s+error:|object\\s+'mtcars_missing_col_expr'\\s+not\\s+found"
-  )
-})
-
-mtcars_random_arg <- "filter('mpg'), mpg, cyl, starts_with('g'), ends_with('b')"
-mtcars_random_str <- .make_tidyselect_arg(.data = "mtcars", fun = "select", tidy_select_args = mtcars_random_arg)
-mtcars_random_expr <- rlang::parse_expr(mtcars_random_str)
-
-test_that(".tidy_eval_handle_errors fails with correct message when unrecognised function passed", {
-  expect_error(
-    .tidy_eval_handle_errors(mtcars_random_expr, "mtcars"),
-    "`selectDS`\\s+returned\\s+the\\s+following\\s+error:|object\\s+'mtcars_random_expr'\\s+not\\s+found"
-  )
-})
-
 test_that(".get_encode_dictionary returns the expected encoding key", {
   expected_encode_list <- list(
     input = c("(", ")", "\"", ",", " ", ":", "!", "&", "|", "'", "[", "]", "=", "+", "-", "*", "/", "^"),
