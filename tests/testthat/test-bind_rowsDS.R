08398d6b81f45da17bdf48a9a47a9594ba2708e5
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-07-15 15:01:06 +0200

    test: added tests for bind_rows

---
+++ tests/testthat/test-bind_rowsDS.R
@@ -0,0 +1,86 @@
+library(DSLite)
+library(dplyr)
+library(dsTidyverse)
+library(dsBase)
+library(dsBaseClient)
+library(purrr)
+
+options(datashield.env = environment())
+data("mtcars")
+dslite.server <- DSLite::newDSLiteServer(tables = list(mtcars = mtcars))
+data("logindata.dslite.cnsim")
+logindata.dslite.cnsim <- logindata.dslite.cnsim %>%
+  mutate(table = "mtcars")
+dslite.server$config(defaultDSConfiguration(include = c("dsBase", "dsTidyverse")))
+dslite.server$assignMethod("bind_rowsDS", "dsTidyverse::bind_rowsDS")
+dslite.server$aggregateMethod("exists", "base::exists")
+dslite.server$aggregateMethod("classDS", "dsBase::classDS")
+dslite.server$aggregateMethod("lsDS", "dsBase::lsDS")
+dslite.server$aggregateMethod("dsListDisclosureSettingsTidyVerse", "dsTidyverse::dsListDisclosureSettingsTidyVerse")
+conns <- datashield.login(logins = logindata.dslite.cnsim, assign = TRUE)
+
+
+test_that("bind_rowsDS passes", {
+
+  bind_rows_arg <- "mtcars, mtcars"
+  other_args <- ".id = NULL"
+  bind_rows_cally <- .make_tidyverse_call(NULL, "bind_rows", bind_rows_arg, other_args, inc_data = F)
+  result <- eval(bind_rows_cally)
+
+  expect_equal(
+    class(result),
+    "data.frame"
+    )
+
+  expect_equal(
+    colnames(result),
+    c("mpg", "cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb")
+  )
+
+  expect_equal(
+    dim(result),
+    c(64, 11)
+  )
+
+  expect_equal(
+    result %>% map_dbl(mean) %>% round(2),
+    c(mpg = 20.09, cyl = 6.19, disp = 230.72, hp = 146.69, drat = 3.60, wt = 3.22, qsec = 17.85, vs = 0.44, am = 0.41, gear = 3.69, carb = 2.81)
+  )
+
+})
+
+  test_that("bind_rowsDS passes with .id argument", {
+
+    bind_rows_arg <- "mtcars, mtcars"
+    other_args <- ".id = 'test_col_name'"
+    bind_rows_cally <- .make_tidyverse_call(NULL, "bind_rows", bind_rows_arg, other_args, inc_data = F)
+    result <- eval(bind_rows_cally)
+
+    expect_equal(
+      colnames(result),
+      c("test_col_name", "mpg", "cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb")
+    )
+
+  })
+
+test_that("bind_rowsDS passes when called directly", {
+  cally <- call("bind_rowsDS", "mtcars$COMMA$$SPACE$mtcars", NULL)
+  datashield.assign(conns, "test", cally)
+
+
+  expect_equal(
+    ds.class("test")[[1]],
+    "data.frame"
+  )
+
+  expect_equal(
+    ds.colnames("test")[[1]],
+    c("mpg", "cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb")
+  )
+
+  expect_equal(
+    ds.dim("test")[[1]],
+    c(64, 11)
+  )
+
+})
