422fc68731f4b44a6fffc07a6c588288d5f97ad0
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-07-23 15:56:09 +0200

    feat: added bind_cols

---
+++ R/bind_colsDS.R
@@ -0,0 +1,17 @@
+#' @title Bind multiple data frames by column
+#' @description DataSHIELD implementation of \code{dplyr::bind_cols}.
+#' @param to_combine 	Data frames to combine. Each argument can either be a data frame, a list that
+#' could be a data frame, or a list of data frames. Columns are matched by name, and any missing
+#' columns will be filled with NA.
+#' @param .name_repair One of "unique", "universal", or "check_unique". See
+#' \code{vctrs::vec_as_names()} for the meaning of these options.
+#' @return A serverside data frame with name specified in \code{newobj} and the same type as the
+#' first element of `to_combine`.
+#' @export
+bind_colsDS <- function(to_combine = NULL, .name_repair = NULL) {
+  to_combine <- .decode_tidy_eval(to_combine, .get_encode_dictionary())
+  other_args <- .paste_character_args(.name_repair)
+  call <- .make_tidyverse_call(.data = NULL, "bind_cols", to_combine, other_args, inc_data = F)
+  out <- .execute_with_error_handling("bind_cols", call)
+  return(out)
+}
---
+++ tests/testthat/test-bind_colsDS.R
@@ -0,0 +1,115 @@
+library(DSLite)
+library(dplyr)
+library(dsTidyverse)
+library(dsBase)
+library(dsBaseClient)
+library(purrr)
+
+options(datashield.env = environment())
+data("mtcars")
+dslite.server <- DSLite::newDSLiteServer(tables = list(mtcars = mtcars))
+data("logindata.dslite.cnsim")
+logindata.dslite.cnsim <- logindata.dslite.cnsim %>%
+  mutate(table = "mtcars")
+dslite.server$config(defaultDSConfiguration(include = c("dsBase", "dsTidyverse")))
+dslite.server$assignMethod("bind_colsDS", "dsTidyverse::bind_colsDS")
+dslite.server$aggregateMethod("exists", "base::exists")
+dslite.server$aggregateMethod("classDS", "dsBase::classDS")
+dslite.server$aggregateMethod("lsDS", "dsBase::lsDS")
+dslite.server$aggregateMethod("dsListDisclosureSettingsTidyVerse", "dsTidyverse::dsListDisclosureSettingsTidyVerse")
+conns <- datashield.login(logins = logindata.dslite.cnsim, assign = TRUE)
+
+
+test_that("bind_colsDS passes", {
+
+  bind_cols_arg <- "mtcars, mtcars"
+  other_args <- '.name_repair = c("unique", "universal", "check_unique", "minimal")'
+  bind_cols_cally <- .make_tidyverse_call(NULL, "bind_cols", bind_cols_arg, other_args, inc_data = F)
+  result <- eval(bind_cols_cally)
+
+  expect_equal(
+    class(result),
+    "data.frame"
+    )
+
+  expect_equal(
+    colnames(result),
+    c("mpg...1", "cyl...2", "disp...3", "hp...4", "drat...5", "wt...6", "qsec...7", "vs...8",
+      "am...9", "gear...10", "carb...11", "mpg...12", "cyl...13", "disp...14", "hp...15",
+      "drat...16", "wt...17", "qsec...18", "vs...19", "am...20", "gear...21", "carb...22")
+  )
+
+  expect_equal(
+    dim(result),
+    c(32, 22)
+  )
+
+})
+
+  test_that("bind_rowsDS passes with .name_repair argument", {
+
+    bind_cols_arg <- "mtcars, mtcars"
+    other_args <- '.name_repair = "unique"'
+    bind_cols_cally <- .make_tidyverse_call(NULL, "bind_cols", bind_cols_arg, other_args, inc_data = F)
+    result <- eval(bind_cols_cally)
+
+    expect_equal(
+      colnames(result),
+      c("mpg...1", "cyl...2", "disp...3", "hp...4", "drat...5", "wt...6", "qsec...7", "vs...8",
+        "am...9", "gear...10", "carb...11", "mpg...12", "cyl...13", "disp...14", "hp...15",
+        "drat...16", "wt...17", "qsec...18", "vs...19", "am...20", "gear...21", "carb...22")
+    )
+
+    other_args <- '.name_repair = "universal"'
+    bind_cols_cally <- .make_tidyverse_call(NULL, "bind_cols", bind_cols_arg, other_args, inc_data = F)
+    result <- eval(bind_cols_cally)
+
+    expect_equal(
+      colnames(result),
+      c("mpg...1", "cyl...2", "disp...3", "hp...4", "drat...5", "wt...6", "qsec...7", "vs...8",
+        "am...9", "gear...10", "carb...11", "mpg...12", "cyl...13", "disp...14", "hp...15",
+        "drat...16", "wt...17", "qsec...18", "vs...19", "am...20", "gear...21", "carb...22")
+    )
+
+    other_args <- '.name_repair = "check_unique"'
+    bind_cols_cally <- .make_tidyverse_call(NULL, "bind_cols", bind_cols_arg, other_args, inc_data = F)
+
+    expect_error(
+      eval(bind_cols_cally),
+      "Names must be unique."
+    )
+
+    other_args <- '.name_repair = "minimal"'
+    bind_cols_cally <- .make_tidyverse_call(NULL, "bind_cols", bind_cols_arg, other_args, inc_data = F)
+    result <- eval(bind_cols_cally)
+
+    expect_equal(
+      colnames(result),
+      c("mpg", "cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb",
+        "mpg", "cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb")
+    )
+
+})
+
+test_that("bind_colsDS passes when called directly", {
+  cally <- call("bind_colsDS", "mtcars$COMMA$$SPACE$mtcars", c("unique", "universal", "check_unique", "minimal"))
+  datashield.assign(conns, "test", cally)
+
+  expect_equal(
+    ds.class("test")[[1]],
+    "data.frame"
+  )
+
+  expect_equal(
+    ds.colnames("test")[[1]],
+    c("mpg...1", "cyl...2", "disp...3", "hp...4", "drat...5", "wt...6", "qsec...7", "vs...8",
+      "am...9", "gear...10", "carb...11", "mpg...12", "cyl...13", "disp...14", "hp...15",
+      "drat...16", "wt...17", "qsec...18", "vs...19", "am...20", "gear...21", "carb...22")
+  )
+
+  expect_equal(
+    ds.dim("test")[[1]],
+    c(32, 22)
+  )
+
+})
