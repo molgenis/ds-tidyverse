7dbc42b95bafab747bb269d04d40d190eaaffc3b
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-07-23 15:50:56 +0200

    feat: bind rows (#43)
    * feat: added bind_rows
    
    * chore: updated exports
    
    * docs: updated manual
    
    * refactor: changed name to camel case
    
    * chore: corrected ci file
    
    * amended following review

---
+++ R/bind_rowsDS.R
@@ -0,0 +1,18 @@
+#' @title Bind multiple data frames by row.
+#' @description DataSHIELD implementation of \code{dplyr::bind_rows}.
+#' @param to_combine 	Data frames to combine. Each argument can either be a data frame, a list that
+#' could be a data frame, or a list of data frames. Columns are matched by name, and any missing
+#' columns will be filled with NA.
+#' @param .id he name of an optional identifier column. Provide a string to create an output column
+#' that identifies each input. The column will use names if available, otherwise it will use
+#' positions.
+#' @return A serverside data frame with name specified in \code{newobj} and the same type as the
+#' first element of `to_combine`.
+#' @export
+bindRowsDS <- function(to_combine = NULL, .id = NULL) {
+  to_combine <- .decode_tidy_eval(to_combine, .get_encode_dictionary())
+  other_args <- .paste_character_args(.id)
+  call <- .make_tidyverse_call(.data = NULL, "bind_rows", to_combine, other_args, inc_data = F)
+  out <- .execute_with_error_handling("bind_rows", call)
+  return(out)
+}
