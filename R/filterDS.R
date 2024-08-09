43ee786663545568b9a1ff4244a7a7af6a0deab4
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-08-09 16:46:27 +0200

    feat: finished filterDS

--- R/filterDS.R
+++ R/filterDS.R
@@ -1,18 +1,18 @@
 #' @title Performs dplyr filter
 #' @description This function is similar to R function \code{filter}.
 #' @details Performs dplyr filter
+#' @param expression Diffused expression of dotdotdot passed to ds.filter
 #' @param .data A data frame, data frame extension (e.g. a tibble), or a lazy data frame
 #' (e.g. from dbplyr or dtplyr).
-#' @param expression Diffused expression of dotdotdot passed to ds.filter
 #' @param .preserve Relevant when the .data input is grouped. If .preserve = FALSE (the default),
 #' the grouping structure is recalculated based on the resulting data, otherwise the grouping is kept as is.
 #' @return the object specified by the \code{newobj} argument of \code{ds.filter} which is written
 #' to the serverside.
 #' @export
-filterDS <- function(expr, .data, .keep = NULL, .before = NULL, .after = NULL) {
+filterDS <- function(expr, .data, .preserve = NULL) {
   tidy_select <- .decode_tidy_eval(expr, .get_encode_dictionary())
-  other_args <- .paste_character_args(.keep, .before, .after)
-  call <- .make_tidyverse_call(.data, "mutate", tidy_select, other_args)
-  out <- .execute_with_error_handling("mutate", call)
+  other_args <- .paste_character_args(.preserve)
+  call <- .make_tidyverse_call(.data, "filter", tidy_select, other_args)
+  out <- .execute_with_error_handling("filter", call)
   return(out)
 }
