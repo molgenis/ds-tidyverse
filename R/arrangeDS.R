876073925cb80ed20a1dbdc8a56cbef65718ff75
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-09-10 09:53:50 +0200

    feat: first commimt arrange

---
+++ R/arrangeDS.R
@@ -0,0 +1,16 @@
+#' @title Order the rows of a data frame by the values of selected columns
+#' @description DataSHIELD implentation of  \code{dplyr::arrange}.
+#' @param .data A data frame, data frame extension (e.g. a tibble), or a lazy data frame (e.g. from dbplyr or dtplyr).
+#' @param expr Variables, or functions of variables. Use desc() to sort a variable in descending
+#' order.
+#' @param .by_group If TRUE, will sort first by grouping variable. Applies to grouped data frames
+#' only.
+#' @return the object specified by the \code{newobj} argument of \code{ds.arrange} which is written
+#' to the serverside.
+#' @export
+arrangeDS <- function(expr, .data, .by_group) {
+  tidy_select <- .decode_tidy_eval(expr, .get_encode_dictionary())
+  call <- .make_tidyverse_call(.data, "arrange", tidy_select, list(.by_group))
+  out <- .execute_with_error_handling("arrange", call)
+  return(out)
+}
