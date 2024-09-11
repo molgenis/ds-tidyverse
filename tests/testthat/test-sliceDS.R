c0e4dda83b57eb091890b96d0a316f5e15311c4c
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-09-11 10:12:52 +0200

    test: added tests for slice

---
+++ tests/testthat/test-sliceDS.R
@@ -0,0 +1,94 @@
+library(DSLite)
+library(dplyr)
+library(dsTidyverse)
+library(dsBase)
+library(dsBaseClient)
+
+options(datashield.env = environment())
+data("mtcars")
+dslite.server <- DSLite::newDSLiteServer(tables = list(mtcars = mtcars))
+data("logindata.dslite.cnsim")
+logindata.dslite.cnsim <- logindata.dslite.cnsim %>%
+  mutate(table = "mtcars")
+dslite.server$config(defaultDSConfiguration(include = c("dsBase", "dsTidyverse", "dsDanger")))
+dslite.server$assignMethod("sliceDS", "dsTidyverse::sliceDS")
+dslite.server$aggregateMethod("exists", "base::exists")
+dslite.server$aggregateMethod("classDS", "dsBase::classDS")
+dslite.server$aggregateMethod("lsDS", "dsBase::lsDS")
+dslite.server$aggregateMethod("dsListDisclosureSettingsTidyVerse", "dsTidyverse::dsListDisclosureSettingsTidyVerse")
+conns <- datashield.login(logins = logindata.dslite.cnsim, assign = TRUE)
+
+mtcars_group <- mtcars %>% group_by(carb)
+
+good_slice_arg <- "1:5"
+
+test_that("sliceDS correctly filters where data and columns exist", {
+  good_slice_cally <- .make_tidyverse_call("mtcars", "slice", good_slice_arg)
+    expect_equal(
+    dim(eval(good_slice_cally)),
+    c(5, 11)
+  )
+})
+
+test_that("sliceDS works with .by argument", {
+
+  by_cally <- .make_tidyverse_call("mtcars", "slice", "1:5", ".by = 'cyl'")
+  no_by_cally <- .make_tidyverse_call("mtcars", "slice", "1:5")
+
+  slice_with_by <- eval(by_cally)
+  slice_without_by <- eval(no_by_cally)
+
+  expect_false(
+    identical(
+      rownames(slice_with_by),
+      rownames(slice_without_by)
+      )
+  )
+
+  expect_equal(
+    dim(slice_with_by),
+    c(15, 11))
+
+  expect_equal(
+    dim(slice_without_by),
+    c(5, 11))
+
+})
+
+test_that("sliceDS works with .preserve argument", {
+
+  preserve_cally_true <- .make_tidyverse_call("mtcars_group", "slice", "9:12", ".preserve = TRUE")
+  expect_equal(
+    group_keys(eval(preserve_cally_true)),
+    tibble(carb = c(1, 2, 3, 4, 6, 8))
+  )
+
+  preserve_cally_false <- .make_tidyverse_call("mtcars_group", "slice", "9:12", ".preserve = FALSE")
+  expect_equal(
+    group_keys(eval(preserve_cally_false)),
+    tibble(carb = c(2, 4))
+  )
+})
+
+test_that("sliceDS fails when data doesn't exist", {
+  no_data <- .make_tidyverse_call("doesntexist", "slice", good_slice_arg)
+  expect_error(
+    eval(no_data),
+    "object 'doesntexist' not found"
+  )
+})
+
+test_that("sliceDS passes when called directly", {
+  cally <- call("sliceDS", "1:5", "mtcars", NULL, FALSE)
+
+  datashield.assign(conns, "test", cally)
+
+  expect_equal(
+    ds.class("test")[[1]],
+    "data.frame")
+
+  expect_equal(
+    ds.dim("test")[[1]],
+    c(5, 11)
+  )
+})
