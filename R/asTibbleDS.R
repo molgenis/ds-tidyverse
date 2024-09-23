a5b3d71b1ce46a17c59278e1cd0fe5914dfa85c7
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-09-23 11:14:58 +0200

    docs: tidied up help file

--- R/asTibbleDS.R
+++ R/asTibbleDS.R
@@ -5,17 +5,20 @@
 #' @param .rows The number of rows, useful to create a 0-column tibble or just as an additional
 #' check.
 #' @param .name_repair Treatment of problematic column names:
-#'   * "minimal": No name repair or checks, beyond basic existence,\
-#'   * "unique": Make sure names are unique and not empty,
-#'   * "check_unique": (default value), no name repair, but check they are unique,
-#'   * "universal": Make the names unique and syntactic
+#'   \itemize{
+#'     \item "minimal": No name repair or checks, beyond basic existence
+#'     \item "unique": Make sure names are unique and not empty
+#'     \item "check_unique": (default value), no name repair, but check they are unique
+#'     \item "universal": Make the names unique and syntactic
+#'   }
 #' @param rownames How to treat existing row names of a data frame or matrix:
-#'   * `NULL`: remove row names. This is the default.
-#'   * `NA`: keep row names.
-#'   * A string: the name of a new column. Existing rownames are transferred
-#'     into this column and the `row.names` attribute is deleted.
-#'     No name repair is applied to the new column name, even if `x` already contains
-#'     a column of that name.
+#'   \itemize{
+#'     \item `NULL`: remove row names. This is the default.
+#'     \item `NA`: keep row names.
+#'     \item A string: the name of a new column. Existing rownames are transferred
+#'     into this column and the `row.names` attribute is deleted. No name repair is applied
+#'     to the new column name, even if `x` already contains a column of that name.
+#'   }
 #' @param column_name Optionally, specify column names of the new object.
 #' @return the object specified by the \code{newobj} argument of \code{ds.as_tibble} which is
 #' written to the serverside.
