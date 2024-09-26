548dba86a9357f5913a5e0b2a429ffb7e08bd130
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-09-26 12:26:18 +0200

    refactor: remove column_name argument as no longer supporting this functionality as only relevant for converting vectors or lists which we're not currently supporting

--- R/asTibbleDS.R
+++ R/asTibbleDS.R
@@ -19,12 +19,11 @@
 #'     into this column and the `row.names` attribute is deleted. No name repair is applied
 #'     to the new column name, even if `x` already contains a column of that name.
 #'   }
-#' @param column_name Optionally, specify column names of the new object.
 #' @return the object specified by the \code{newobj} argument of \code{ds.as_tibble} which is
 #' written to the serverside.
 #' @export
-asTibbleDS <- function(x, .rows, .name_repair, rownames, column_name) {
-  other_args <- .paste_character_args(.rows, .name_repair, rownames, column_name)
+asTibbleDS <- function(x, .rows, .name_repair, rownames) {
+  other_args <- .paste_character_args(.rows, .name_repair, rownames)
   call <- .make_tidyverse_call(x, "as_tibble", other_args)
   out <- .execute_with_error_handling("as_tibble", call)
   .check_subset_disclosure_risk(eval(parse(text = x), envir = parent.frame()), out)
