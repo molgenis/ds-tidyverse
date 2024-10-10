e9d9b42d227ad897213f8c0a7802b7299d969cf7
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-10-10 10:20:16 +0200

    docs: added back argument I deleted

--- R/mutateDS.R
+++ R/mutateDS.R
@@ -1,7 +1,9 @@
 #' @title Create, modify, and delete columns
 #' @description DataSHIELD implentation of \code{mutate}.
-#' @param .data A data frame, data frame extension (e.g. a tibble), or a lazy data frame (e.g. from dbplyr or dtplyr).
+#' @param df.name A data frame, data frame extension (e.g. a tibble), or a lazy data frame (e.g. from dbplyr or dtplyr).
 #' @param tidy_expr Name-value pairs. The name gives the name of the column in the output.
+#' @param .keep .keep Control which columns from \code{df.name} are retained in the output. Grouping
+#' columns and columns created by \code{tidy_expr} are always kept.
 #' \itemize{
 #'   \item \code{"all"}: Retains all columns from \code{df.name}. This is the default.
 #'   \item \code{"used"}: Retains only the columns used in \code{tidy_expr} to create new columns.
@@ -16,12 +18,12 @@
 #' @return the object specified by the \code{newobj} argument of \code{ds.mutate} which is written
 #' to the serverside.
 #' @export
-mutateDS <- function(tidy_expr, .data, .keep = NULL, .before = NULL, .after = NULL) {
+mutateDS <- function(tidy_expr, df.name, .keep = NULL, .before = NULL, .after = NULL) {
   checkPermissivePrivacyControlLevel(c('permissive', 'banana', 'avacado'))
   tidy_expr <- .decode_tidy_eval(tidy_expr, .get_encode_dictionary())
-  .check_tidy_disclosure(.data, tidy_expr)
+  .check_tidy_disclosure(df.name, tidy_expr)
   other_args <- .paste_character_args(.keep, .before, .after)
-  call <- .make_tidyverse_call(.data, "mutate", tidy_expr, other_args)
+  call <- .make_tidyverse_call(df.name, "mutate", tidy_expr, other_args)
   out <- .execute_with_error_handling("mutate", call)
   return(out)
 }
