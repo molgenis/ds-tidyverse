60cf865478d67b371ff8c96d23c4b7d5c8874cf1
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2025-10-30 12:30:13 +0100

    added additional permitted functions

--- R/utils.R
+++ R/utils.R
@@ -174,7 +174,7 @@ listPermittedTidyverseFunctionsDS <- function() {
     "matches", "num_range", "all_of", "any_of", "where", "rename", "mutate", "if_else",
     "case_when", "mean", "median", "mode", "desc", "last_col", "nth", "where", "num_range",
     "exp", "sqrt", "scale", "round", "floor", "ceiling", "abs", "sd", "var",
-    "sin", "cos", "tan", "asin", "acos", "atan", "c", "as.character", "as.integer",
+    "sin", "cos", "tan", "asin", "acos", "atan", "c", "as.character", "as.integer", "as.numeric",
     "lag", "diff", "cumsum"
   )
 
