02bf50983e1db1bdd7c48feda5392f04ea71cfa5
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2025-11-26 14:09:32 +0100

    docs: removed unnecessary lines from vignette builder

--- vignettes/pre-render.R
+++ vignettes/pre-render.R
@@ -1,5 +1,2 @@
 # To update the vignette:
-knitr::knit("vignettes/dstidyverse.Rmd.orig", output = "vignettes/dstidyverse.Rmd")
-
-# To create a script that uploads the files:
-knitr::purl("vignettes/dstidyverse.Rmd.orig", output = "vignettes/dstidyverse.R")
+knitr::knit("vignettes/tidyversePrivacy.Rmd.orig", output = "vignettes/tidyversePrivacy.Rmd")
